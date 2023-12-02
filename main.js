0aa25e19a4e3096ad6f6036ea6ce29b2ea180179
almasikiric <sial23hk@student.ju.se>
2023-12-02 23:48:06 +0100

    filter

---
+++ main.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    const filterTextButtons = document.getElementById("filter-text");
+    const filterDiv = document.getElementById("inside-filter");
+    let isVisible = false;
+  
+    filterTextButtons.addEventListener("click", function() {
+      if(isVisible) {
+          filterDiv.className="inside-filter filter-hidden";
+          isVisible=false;
+        
+      }
+      else{
+          filterDiv.className="inside-filter"
+          isVisible=true;
+          
+      }
+    });
+  };
