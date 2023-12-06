8815c0703e6bd2095cb485d1027455678443a541
lofquista <loal23gk@student.ju.se>
2023-12-06 23:50:05 +0100

    bit of media query on information page

--- information.js
+++ information.js
@@ -1,5 +1,4 @@
-document
-  .getElementById("select-delivery-location")
-  .addEventListener("click", function () {
-    this.nextElementSibling.classList.toggle("dropdown-show-content");
-  });
+document.getElementById("select-delivery-location");
+document.addEventListener("click", function () {
+  this.nextElementSibling.classList.toggle("dropdown-show-content");
+});
