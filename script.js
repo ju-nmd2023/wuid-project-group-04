a4d5cc50007922a0776830840a2822e38177db4d
linnmoberg <linn1.moberg@gmail.com>
2023-12-07 17:42:37 +0100

    menu changes

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-window.onload = function () {
-    const filterTextButtons = document.getElementById("telephonebutton");
-    const filterDiv = document.getElementById("button-menu")[0];
-    let isVisible = false;
+// window.onload = function () {
+//     const filterTextButtons = document.getElementById("telephonebutton");
+//     const filterDiv = document.getElementsByClassName("button-menu")[0];
+//     let isVisible = false;
  
-    filterTextButtons.addEventListener("click", function() {
-      if(isVisible) {
-          filterDiv.className="button-menu header-button";
-          isVisible=false;
+//     filterTextButtons.addEventListener("click", function() {
+//       if(isVisible) {
+//           filterDiv.className="button-menu header-button";
+//           isVisible=false;
        
-      }
-      else{
-          filterDiv.className="button-menu"
-          isVisible=true;
+//       }
+//       else{
+//           filterDiv.className="button-menu"
+//           isVisible=true;
          
-      }
-    });
-  };
+//       }
+//     });
+//   };
+
+// document
+//   .getElementById("descriptionButton")
+//   .addEventListener("click", function () {
+//     this.nextElementSibling.classList.toggle("dropdown-show-content");
+//   });
+
+// document
+//   .getElementById("careInstructionsButton")
+//   .addEventListener("click", function () {
+//     this.nextElementSibling.classList.toggle("dropdown-show-content");
+//   });
