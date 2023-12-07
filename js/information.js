fdb2950da390cfdd68b63012585ecf5b93620977
lofquista <loal23gk@student.ju.se>
2023-12-07 21:09:46 +0100

    link between pages

--- js/information.js
+++ js/information.js
@@ -1,4 +1,42 @@
+// FOR DROPDOWN MENU ON INFORMATION PAGE //
+
 document.getElementById("select-delivery-location");
 document.addEventListener("click", function () {
   this.nextElementSibling.classList.toggle("dropdown-show-content");
 });
+
+// function redirectToPurchase() {
+//   window.location.href = "purchase.html";
+// }
+
+// function validateForm() {
+//   var email = document.forms["purchaseForm"]["email"].value;
+//   var name = document.forms["purchaseForm"]["name"].value;
+
+//   if (email === "" || name === "") {
+//     alert("All fields must be filled out");
+//     return false;
+//   }
+
+//   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//   if (!emailRegex.test(email)) {
+//     alert("Enter valid email");
+//     return false;
+//   }
+//   return true;
+// }
+
+// document
+//   .getElementById("paymentButton")
+//   .addEventListener("click", validatePurchaseForm);
+
+// function validatePurchaseForm() {
+//   const emailInput = document.getElementById("emailInputField");
+//   const email = emailInput.value;
+//   if (email === "@ .") {
+//     emailInput.classList.remove("redborder");
+//   } else {
+//     emailInput.classList.add("redborder");
+//     alert("Error, use @");
+//   }
+// }
