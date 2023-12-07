462bb1437139c141050a61b3442907cc45ff8958
almasikiric <sial23hk@student.ju.se>
2023-12-07 14:11:02 +0100

    guery
    query

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 window.onload = function () {
-    const filterTextButtons = document.getElementById("filter-text");
-    const filterDiv = document.getElementById("inside-filter");
-    let isVisible = false;
-  
-    filterTextButtons.addEventListener("click", function() {
-      if(isVisible) {
-          filterDiv.className="inside-filter filter-hidden";
-          isVisible=false;
-        
-      }
-      else{
-          filterDiv.className="inside-filter"
-          isVisible=true;
-          
-      }
-    });
-  };
+  const filterTextButtons = document.getElementById("filter-text");
+  const filterDiv = document.getElementsByClassName("filter-hidden")[0];
+  let isVisible = true;
+
+  filterTextButtons.addEventListener("click", function () {
+    if (isVisible) {
+      filterDiv.className = "filter-hidden";
+      isVisible = false;
+    } else {
+      filterDiv.className = "inside-filter";
+      isVisible = true;
+    }
+  });
+};
